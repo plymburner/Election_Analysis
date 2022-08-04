c294b48d6bbd8e07c8d4bce9a865650557341055
Paul Lymburner <30843471+plymburner@users.noreply.github.com>
2022-08-03 21:50:23 -0400

    Final Commits

---
+++ Python_practice.py
@@ -0,0 +1,41 @@
+#print("Hello World")
+
+#print(type(2019))
+
+#print(5 + 2 * 3)
+#print(8 // 5 - 3)
+#print(8 + 22 * 2 - 4)
+#print(16 - 3 / 2 + 7 - 1)
+#print(3 ** 3 % 5)
+#print(5 + 9 * 3 / 2 - 4)
+
+#print((5 + 2) * 3)
+#print((8 // 5) - 3)
+#print(8 + (22 * (2 - 4)))
+#print(16 - 3 / (2 + 7) - 1)
+#print(3 ** (3 % 5))
+#print(5 + (9 * 3 / 2 - 4)) 
+#print(5 + (9 * 3 / (2 - 4)))
+
+#counties = ["Arapahoe","Denver","Jefferson"]
+
+#counties.append("El Paso")
+
+#counties.pop(3)
+
+#print(counties)
+
+#print(len(counties))
+
+#counties_tuple = ("Arapahoe","Denver","Jefferson")
+
+#print(counties_tuple[1:2])
+
+voting_data = []
+
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+voting_data.append({"county":"El Paso", "registered_voters": 461149})
+
+print(voting_data)
